da0c685ec9727df8fd0b7b13f2e464663d5fc273
Long9725 <develop9725@gmail.com>
2022-01-16 15:08:26 +0900

    Lottie assets

--- lib/src/page/getting_started.dart
+++ lib/src/page/getting_started.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:lottie/lottie.dart';
+
 class GettingStartedPage extends StatefulWidget {
   const GettingStartedPage({Key? key, required this.title}) : super(key: key);
 
@@ -10,44 +12,12 @@ class GettingStartedPage extends StatefulWidget {
 }
 
 class _GettingStartedPageState extends State<GettingStartedPage> {
-  bool _show = false;
-
   @override
   Widget build(BuildContext context) {
-    return Visibility(
-      visible: _show,
-      child: Scaffold(
-          body: Container(
-            child: Stack(
-//           mainAxisAlignment: MainAxisAlignment.end,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 400,
-                  width: 400,
-                  color: Colors.amber,
-                  child: Text("Hello world"),
-                ),
-                Positioned(
-                    top: 25,
-                    bottom: 25,
-                    left: 50,
-                    right: 50,
-                    child: Container(
-                      height: 200,
-                      width: 200,
-                      color: Colors.red,
-                      child: Text("Hello world"),
-                    )),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                  child: Text("Hello world"),
-                ),
-              ],
-            ),
-          )),
+    return Scaffold(
+      body: Container(
+        child: Lottie.asset('assets/lotties/getting-started-coch.json'),
+      ),
     );
   }
 }
